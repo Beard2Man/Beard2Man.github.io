14cef54514b5fee5a1bdd948375bc2fa5740dfbe
grzegorz.o <grzegorz.sebastian.olejarczyk@gmail.com>
2023-04-11 13:37:30 +0200

    DOM

---
+++ DOM-Ćwiczenia/DOM-PODSTAWY/js/main.js
@@ -0,0 +1,21 @@
+// window.alert("Hans");
+
+// alert("Hans");
+
+// window.prompt("Podaj swój wiek");
+
+// let age = window.prompt("Podaj swój wiek");
+// console.log("Masz ${age} lat");
+
+// window.confirm("Akceptuje umowę")
+
+// let accept = window.confirm("Akceptuje umowę");
+// if (accept) {
+//   console.log("Umowa zaakceptowana");
+// } else {
+//   console.log("Umowa NIEzaakcpetowana");
+// }
+
+//
+
+console.log("$}");
